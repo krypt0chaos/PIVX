56a1fcc0aebd4500f1b973d2cab7107f5c1b9182
Mrs-X <Mrs-X@users.noreply.github.com>
2017-11-01 12:51:59 +0100

    [UI] Redesign with vertical tabs

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -130,7 +130,6 @@ OverviewPage::OverviewPage(QWidget* parent) : QWidget(parent),
 
     connect(ui->listTransactions, SIGNAL(clicked(QModelIndex)), this, SLOT(handleTransactionClicked(QModelIndex)));
 
-
     // init "out of sync" warning labels
     ui->labelWalletStatus->setText("(" + tr("out of sync") + ")");
     ui->labelTransactionsStatus->setText("(" + tr("out of sync") + ")");
